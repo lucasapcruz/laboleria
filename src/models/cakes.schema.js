144e8a660630b044fb1c4a6ef3b050ee628f166f
Lucas Cruz <lucas.apcruz@gmail.com>
2023-01-05 23:19:32 -0300

    fix: cake schema

--- src/models/cakes.schema.js
+++ src/models/cakes.schema.js
@@ -3,5 +3,8 @@ import dateExtension from "@joi/date";
 const joi = baseJoi.extend(dateExtension);
 
 export const cakesSchema = joi.object({
-    name: joi.string().required().min(2)
+    name: joi.string().required().min(2),
+    price: joi.number().precision(2).required(),
+    image: joi.string().uri().required(),
+    description: joi.string().required().min(2)
 });
