fbaa7829be0900135cb27f417c686e70079c8f8f
Lucas Cruz <lucas.apcruz@gmail.com>
2023-01-08 22:47:43 -0300

    fix: http request error messages

--- src/middlewares/schemaValidation.middleware.js
+++ src/middlewares/schemaValidation.middleware.js
@@ -2,13 +2,17 @@ export function validateSchema(schema) {
   return (req, res, next) => {
     const payload = req.body;
 
-    const { error } = schema.validate(payload, { abortEarly: false });
+    const { error } = schema.validate(payload, { abortEarly: true });
 
-    if (error) {
-      const errors = error.details.map((detail) => detail.message);
-      return res.status(400).send(errors);
+    if(error){
+      const errorMessages = error.details.map((detail) => detail.message);
+      if(errorMessages[0].includes("image")){
+        return res.status(422).send(errorMessages);
+      }else{
+        return res.status(400).send(errorMessages);
+      }
     }
-
+    
     next();
   }
 }
